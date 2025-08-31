74f793e911ade855de345ec6ada2fa4667ad59eb
tnlake <tn.tech@icloud.com>
2025-08-31 20:28:18 +0900

    v1.0.12 - Fix L10nService duplicate instances by removing from L10nModule providers

--- src/l10n/l10n.module.ts
+++ src/l10n/l10n.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { L10nComponent } from './l10n.component';
-import { L10nService } from './l10n.service';
 import { L10nPipe } from './l10n.pipe';
 import { L10nDirective } from './l10n.directive';
 
@@ -9,6 +8,5 @@ import { L10nDirective } from './l10n.directive';
   declarations: [L10nComponent, L10nPipe, L10nDirective],
   imports: [CommonModule],
   exports: [L10nComponent, L10nPipe, L10nDirective],
-  providers: [L10nService],
 })
 export class L10nModule {}
